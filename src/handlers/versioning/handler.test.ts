3f67a67dcd362cf0fc6e482c59d8f4dfae9cf0d2
Danuel <public.danuel@gmail.com>
2019-08-11 14:43:58 +0900

    각 API의 로직에 대한 테스트 코드 추가

--- src/handlers/versioning/handler.test.ts
+++ src/handlers/versioning/handler.test.ts
@@ -2,17 +2,23 @@ import chai from 'chai'
 
 import { version } from '../../../package.json'
 import { Request, Response } from '../../tools'
+import { validateRefreshToken } from '../../middleware'
 import { handler } from './handler'
 
 type Handler = import('../../types').Handler
 
 describe('versioning', () => {
-  it('Response 객체의 body에 현재 버전을 담고 있다', async () => {
-    const request = Request.Empty as Handler['request']
-    const response = Response.Empty
+  const SECRET_KEY = 'Secret Key'
+  validateRefreshToken.initialize(SECRET_KEY)
 
-    await handler(request, response)
+  describe('응답 검증', () => {
+    it('body에 현재 버전을 담고 있다', async () => {
+      const request = Request.Empty as Handler['request']
+      const response = Response.Empty
 
-    chai.should().equal(response.body.raw, version)
+      await handler(request, response)
+
+      chai.should().equal(response.body.raw, version)
+    })
   })
 })
