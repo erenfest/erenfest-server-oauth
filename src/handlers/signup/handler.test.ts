720e33cf13896dc6f713c9d5d0dc94d5a8740f1d
Danuel <public.danuel@gmail.com>
2019-08-14 00:39:45 +0900

    회원가입에 테스트 코드 추가

--- src/handlers/signup/handler.test.ts
+++ src/handlers/signup/handler.test.ts
@@ -4,25 +4,25 @@ describe.skip('signup', () => {
   const SECRET_KEY = 'Secret Key'
   validateRefreshToken.initialize(SECRET_KEY)
 
-  describe('요청 검증', () => {
-    it('제공자provider가 있는지 확인한다')
+  it('제공자provider가 있는지 확인한다')
 
-    it('아이디가 있는지 확인한다')
+  it('아이디가 있는지 확인한다')
 
-    it('암호가 있는지 확인한다')
+  it('암호가 있는지 확인한다')
 
-    it('이메일이 있는지 확인한다')
+  it('이메일이 있는지 확인한다')
 
-    it('정책 동의 리스트가 있는지 확인한다')
-  })
+  it('정책 동의 리스트가 있는지 확인한다')
 
-  describe('응답 검증', () => {
-    it('cookie에 Refresh Token이 있다')
+  it('해당 제공자에 중복 이메일인지 확인힌다')
 
-    it('cookie에 Access Token이 있다')
+  it('해당 제공자에 중복 별칭이 있는지 확인한다')
 
-    it('body에 Refresh Token이 있다')
+  it('cookie에 Refresh Token이 있다')
 
-    it('body에 Access Token이 있다')
-  })
+  it('cookie에 Access Token이 있다')
+
+  it('body에 Refresh Token이 있다')
+
+  it('body에 Access Token이 있다')
 })
